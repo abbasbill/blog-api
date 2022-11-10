c5233bac33ad02a9987aa94099ce65c325a57f6d
abbasbill <abbasbilliamin2@gmail.com>
2022-11-10 10:14:32 +0100

    ---

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = (url) => {
-	return mongoose.connect(url, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	});
+	return mongoose.connect(url);
 };
 
 module.exports = connectDB;
